3b012df92de56d3922421a7d735e125288686aa8
leviychuvak <anton1234maistrenko@gmail.com>
2022-10-02 21:34:44 +0300

    /

---
+++ Begin/GuestBook_Data/GuestBookDataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace GuestBook_Data
+{
+    public class GuestBookDataContext : TableServiceContext
+    {
+        public GuestBookDataContext(string baseAddress, StorageCredentials credentials)
+            : base(baseAddress, credentials)
+        {
+
+        }
+        public IQueryable<GuestBookEntry> GuestBookEntry
+        {
+            get => this.CreateQuery<GuestBookEntry>("GuestBookEntry");
+        }
+    }
+}
---
+++ Begin/GuestBook_WebRole/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace GuestBook_WebRole
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            CloudStorageAccount.SetConfigurationSettingPublisher(
+                (configName, configSetter) =>
+                {
+                    configSetter(RoleEnvironment.GetConfigurationSettingValue(configName));
+                }
+            );
+        }
+    }
+}
